245c3047398431cea0b7ac4401d4f74e485a1a46
Tuan3_PhamHuynhTin <huynhtin0501@gmail.com>
2021-10-19 22:44:49 +0700

    Changed 02

--- js/index.js
+++ js/index.js
@@ -49,50 +49,50 @@ showSlide()
 let products = [
     {
         name: 'JBL E55BT KEY BLACK',
-        image1: './images/JBL_E55BT_KEY_BLACK_6175_FS_x1-1605x1605px.png',
-        image2: './images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
     {
         name: 'JBL JR 310BT',
-        image1: './images/JBL_JR 310BT_Product Image_Hero_Skyblue.png',
-        image2: './images/JBL_JR 310BT_Product Image_Detail_Skyblue.png',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
     {
         name: 'JBL TUNE 750BTNC',
-        image1: './images/kisspng-beats-electronics-headphones-apple-beats-studio-red-headphones.png',
-        image2: './images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
     {
         name: 'JBL Horizon',
-        image1: './images/JBLHorizon_001_dvHAMaster.png',
-        image2: './images/JBLHorizon_BLK_002_dvHAMaster.webp',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
     {
         name: 'JBL Tune 220TWS',
-        image1: './images/JBL_TUNE220TWS_ProductImage_Pink_ChargingCaseOpen.png',
-        image2: './images/JBL_TUNE220TWS_ProductImage_Pink_Back.png',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
     {
         name: 'UA Project Rock',
-        image1: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_HERO.png',
-        image2: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
     {
         name: 'JBL Endurance SPRINT',
-        image1: './images/JBL_Endurance-SPRINT_Product-Image_Red_front-1605x1605px.webp',
-        image2: './images/JBL-Endurance-Sprint_Alt_Red-1605x1605px.webp',
+        image1: './images/xam (4).jpg',
+        image2: './images/xam (3).jpg',
         old_price: '400',
         curr_price: '300'
     },
